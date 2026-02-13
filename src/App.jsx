003c0d0e7e6486afae7d3c0da60e1815e675c72f
vernoxy <vernoxy3@gmail.com>
2026-02-13 16:44:16 +0530

    Lading

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./SubComponents/Navbar";
 import Footer from "./SubComponents/Footer";
-import HomePage from "./Components/HomePage";
-import AboutPage from "./Components/AboutPage";
-import ProductPage from "./Components/ProductPage";
-import ContactPage from "./Components/ContactPage";
 import webbg from "./assets/Webbg.webp";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Aos from "aos";
-import "aos/dist/aos.css"; // Ensure AOS styles are imported
+import "aos/dist/aos.css";
+import Logo from "./assets/KLogo.svg"
+
+// Lazy Load Pages
+const HomePage = lazy(() => import("./Components/HomePage"));
+const AboutPage = lazy(() => import("./Components/AboutPage"));
+const ProductPage = lazy(() => import("./Components/ProductPage"));
+const ContactPage = lazy(() => import("./Components/ContactPage"));
 
 const App = () => {
   useEffect(() => {
-    Aos.init({ duration: 1000,
+    Aos.init({
+      duration: 1000,
       delay: 200,
-      once: true
-     });
+      once: true,
+    });
   }, []);
 
   return (
@@ -26,12 +30,26 @@ const App = () => {
       className="bg-white overflow-x-hidden scroll-smooth text-center"
     >
       <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/products" element={<ProductPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-      </Routes>
+
+      {/* Suspense Wrapper */}
+      <Suspense
+        fallback={
+          <div className="flex flex-col justify-center items-center h-screen text-xl space-y-5">
+
+              <img src={Logo} alt="" loading="lazy" className="animate-pulse"/>
+              <p>Loading...</p>
+
+            </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/products" element={<ProductPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </Suspense>
+
       <Footer />
     </div>
   );
